bf853a15a68fb3dd90ba5bc5004eaff4c79826dd
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-04-09 23:42:02 +0700

    Comparison Operation

---
+++ comparison-operator/compare.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Rivaldhi"
+	var name2 = "Rivaldhi"
+	var full_name = "Muhammad Rivaldhi"
+
+	var result bool = name1 == name2
+	fmt.Println("Result : ", result)
+
+	var result2 = name1 == full_name
+	fmt.Println("Resul 2 : ", result2)
+
+	var result3 = name2 != full_name
+	fmt.Println("Result 3 : ", result3)
+
+	var a = 10
+	var b = 20
+	var c = a < b
+	fmt.Println("Result 4 : ", c)
+
+	var res = a > b
+	fmt.Println("Result 5 : ", res)
+}
